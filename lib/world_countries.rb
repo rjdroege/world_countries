abbb2ee1a44e079f3f0a09dce246c8be858bc2cc
rjdroege <99696219+rjdroege@users.noreply.github.com>
2023-01-16 20:01:15 -0600

    project_start

---
+++ lib/world_countries.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "world_countries/version"
+
+module WorldCountries
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/world_countries/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module WorldCountries
+  VERSION = "0.1.0"
+end
